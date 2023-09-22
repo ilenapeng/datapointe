9d2d9205f1456cd07a5402ce6770c53c5d05dc93
Ilena Peng <30391956+ilenapeng@users.noreply.github.com>
2023-09-21 23:43:48 -0400

    balanchine etc. update

---
+++ project-details/balanchine/chart.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(extrafont)
+
+df <- read_csv("~/Documents/github/balanchine/balanchine.csv")
+
+# reorder composer eras
+
+df$composer_era_factor <- factor(df$`Composer era`,
+                                 levels=unique(df_ordered$my_variable))
+
+plot <- df %>%
+  mutate(composer_era_factor = fct_relevel(`Composer era`, c("Baroque", "Classical", "Romantic", "Modernist", "Neoclassical", "Impressionist", "Contemporary"))) %>%
+  ggplot() + 
+  geom_hline(yintercept=1948, color="gray", size=0.5, linetype='dashed') + 
+  geom_point(aes(x=composer_era_factor, y=Year, size=Dancers, color=composer_era_factor, stroke=NA), alpha=0.7) +
+  scale_y_continuous(trans ='reverse', breaks=seq(1928, 1982, by = 18)) +
+  scale_size_continuous(range = c(0.5,8)) +
+  scale_color_manual(values=c("#F10D03", "#FA7D02", "#FECB18", "#95C940", "#53DFF0", "#6B53C8", "#FDABAC")) +
+  labs(
+    title="Mr. B's Ballets",
+    subtitle="George Balanchine's works & the music he choreographed to\nDot size corresponds to number of dancers",
+    caption="Data from The George Balanchine Trust | Chart by Ilena Peng",
+    x="Musical era",
+    y="Year created"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.background=element_rect(fill="white", color="white"),
+    panel.grid.major.x = element_blank(),
+    panel.grid.minor = element_blank(),
+    text=element_text(family="Baskerville", size=16),
+    axis.title=element_blank(),
+    legend.position="none",
+    plot.caption=element_text(hjust=0.5, size=10),
+    plot.title=element_text(hjust=0.5, face="bold", size=22),
+    plot.subtitle=element_text(hjust=0.5, size=14),
+    plot.margin=margin(1,1,1,1, unit="cm")
+  )
+
+print(plot)
+
+ggsave("~/Documents/github/balanchine/balanchine_raw.png", width=9, height=10, unit="in")
